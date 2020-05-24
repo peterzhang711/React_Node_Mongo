9ca8978c7bf327ca340b008585703c3783a60947
peterzhang711 <raben4@hotmail.com>
2020-05-24 22:22:01 +1000

    1.0

---
+++ redux/store.js
@@ -0,0 +1,11 @@
+// manage the state
+
+import {createStore,applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
+import {composeWithDevTools} from 'redux-devtools-extension'
+
+import reducers from './reducers'
+
+
+
+export default createStore(reducers,composeWithDevTools(applyMiddleware(thunk)))
