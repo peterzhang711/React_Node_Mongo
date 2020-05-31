9d3accd16eb4f0acb2af1c57bc88c5132426e4dc
peterzhang711 <raben4@hotmail.com>
2020-05-31 22:11:26 +1000

    ajax

---
+++ api/ajax.js
@@ -0,0 +1,25 @@
+// ajax
+import axios from 'axios'
+
+export default function ajax(url, data={},method='GET'){
+  if(type==='get'){
+
+    let paramStr = ''
+
+    Object.keys(data).forEach(key => {
+      paramStr += key + '='+ data[key] + '&'
+    })
+
+    if(paramStr){ /
+      paramStr = paramStr.substring(0,paramStr.length-1)
+    }
+    return axios.get(url + '?'+ paramStr)
+  }else {
+    return axios.post(url,data)
+  }
+}
+
+
+
+
+
